6adec376b0e8bedd143a9bb7cb3780745aa969ab
sunamo.cz <radek.jancik@sunamo.cz>
2024-12-25 09:12:41 +0100

    Added SunamoDotNetZip as submodule

--- SunamoDotNetZip/Zip/Deflate64/FileFormats.cs
+++
@@ -1,8 +0,0 @@
-namespace Ionic.Zip.Deflate64;
-    internal interface IFileFormatReader
-    {
-        bool ReadHeader(InputBuffer input);
-        bool ReadFooter(InputBuffer input);
-        void UpdateWithBytesRead(byte[] buffer, int offset, int bytesToCopy);
-        void Validate();
-    }
