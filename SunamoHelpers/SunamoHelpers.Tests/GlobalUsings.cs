85a787dfa4342102577648cc669a96204db98846
smutekutek <smutekutek@gmail.com>
2026-06-21 16:02:13 +0200

    26.6.21.1 y

--- SunamoHelpers/SunamoHelpers.Tests/GlobalUsings.cs
+++ SunamoHelpers/SunamoHelpers.Tests/GlobalUsings.cs
@@ -1,4 +1,7 @@
 // variables names: ok
 global using Xunit;
 global using SunamoHelpers.Helpers;
+global using SunamoHelpers.Tests._sunamo;
+global using System;
 global using System.Collections.Generic;
+global using System.Text.RegularExpressions;
